2a240d93c524daa8cfea5a969143c989bbddce1f
Victor Avila <victoravilamorales95@gmail.com>
2025-02-07 20:21:32 -0700

    feat: redirect if not logged from account page

--- src/app/(site)/account/_components/account-sections.tsx
+++ src/app/(site)/account/_components/account-sections.tsx
@@ -1,11 +1,18 @@
 'use client';
 
+import { redirect } from 'next/navigation';
 import { useIsMounted } from '~/hooks/use-is-mounted';
+import { useUser } from '~/hooks/useUser';
 import { AccountProfile } from './account-profile';
 import { SubscriptionBilling } from './subscription-billing';
 
 export function AccountSections() {
+  const { user } = useUser();
   const isMounted = useIsMounted();
+
+  if (!user) {
+    redirect('/');
+  }
 
   if (!isMounted) return null;
 
