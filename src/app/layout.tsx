2e65cdccafd2f8558ee027cec4cd3381827ca11a
Victor Avila <victoravilamorales95@gmail.com>
2025-02-09 19:31:40 -0700

    feat: layout in app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
-import { Metadata, Viewport } from 'next';
 import './globals.css';
 
+import { Metadata, Viewport } from 'next';
 import { Figtree } from 'next/font/google';
-
 import { PropsWithChildren } from 'react';
+
 import { LayoutFeature } from '~/features/layout/layout';
-import { PlayerFeature } from '~/features/player/player';
 import { cn } from '~/lib/cn';
-import getActiveProductsWithPrices from '~/server/actions/getActiveProductsWithPrices';
 
 const font = Figtree({ subsets: ['latin'] });
 
@@ -15,6 +13,19 @@ const APP_NAME = 'Fountain House Music';
 const APP_DEFAULT_TITLE = 'Fountain House Music';
 const APP_TITLE_TEMPLATE = '%s - Fountain House Music';
 const APP_DESCRIPTION = 'Listen to great music!';
+
+export const revalidate = 0;
+
+export default async function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body suppressHydrationWarning className={cn('dark', font.className)}>
+        <LayoutFeature>{children}</LayoutFeature>
+      </body>
+    </html>
+  );
+}
 
 export const viewport: Viewport = {
   themeColor: '#FFFFFF',
@@ -53,19 +64,3 @@ export const metadata: Metadata = {
     description: APP_DESCRIPTION,
   },
 };
-
-export const revalidate = 0;
-
-export default async function RootLayout({ children }: PropsWithChildren) {
-  const products = await getActiveProductsWithPrices();
-
-  return (
-    <html suppressHydrationWarning lang="en">
-      <head />
-      <body suppressHydrationWarning className={cn('dark', font.className)}>
-        <LayoutFeature>{children}</LayoutFeature>
-        <PlayerFeature />
-      </body>
-    </html>
-  );
-}
