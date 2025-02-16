95648e1a8e93110ccb6cfd6f9358489fc0e7eaed
Victor Avila <victoravilamorales95@gmail.com>
2025-02-15 17:23:03 -0700

    feat: initial safe action setup

---
+++ src/lib/safe-action.tsx
@@ -0,0 +1,34 @@
+import { createSafeActionClient } from 'next-safe-action';
+import { z } from 'zod';
+import { getCurrentUser } from '~/server/actions/user/get-current-user';
+
+export class ActionError extends Error {}
+
+const metadataSchema = z.object({
+  shouldAuth: z.boolean(),
+});
+
+export const actionClient = createSafeActionClient({
+  defaultValidationErrorsShape: 'flattened',
+  defineMetadataSchema: () => metadataSchema,
+  handleServerError: (error) => {
+    if (error instanceof ActionError) {
+      return error.message;
+    }
+
+    // TODO: use generic error message in production
+    return error.message;
+  },
+}).use(async ({ metadata, next }) => {
+  if (metadata?.shouldAuth) {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) {
+      throw new ActionError('You are unauthorized to perform this action');
+    }
+
+    return next({ ctx: { user: currentUser } });
+  }
+
+  return next();
+});
