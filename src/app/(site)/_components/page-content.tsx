b06aa54e80667052d9c6d6ee681558aa5a4d3391
Ken Slachta <ken.slachta@mac.lan>
2025-09-03 09:54:44 -0400

    remove song sort

--- src/app/(site)/_components/page-content.tsx
+++ src/app/(site)/_components/page-content.tsx
@@ -15,10 +15,6 @@ export const PageContent: React.FC<PageContentProps> = ({ albums }) => {
     return <div className="mt-4 text-neutral-400">No albums available.</div>;
   }
 
-  //  const sortedAlbums = [...albums].sort((a, b) => 
-  //   new Date(b.releaseDate).getTime() - new Date(a.releaseDate).getTime()
-  // );
-
   return (
     <div className="mt-4 grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
       {albums.map((album) => (
@@ -26,14 +22,8 @@ export const PageContent: React.FC<PageContentProps> = ({ albums }) => {
           key={album.id}
           album={album}
           onPlayAlbum={() => {
-            // Override the order with the desired order
-            // const overriddenOrder = album.songs.map(song => song.song_order);
-
-            const sortedSongs = album.songs.sort((a, b) => 
-              (a.order || 0) - (b.order || 0)
-            );
-            setSongs(sortedSongs);
-            setCurrentSong(sortedSongs[0]);
+            setSongs(album.songs);
+            setCurrentSong(album.songs[0]);
           }}
         />
       ))}
