91dc38df7e9d43af382a570f15f4be201617657d
Ken Slachta <ken.slachta@mac.lan>
2025-09-07 20:30:39 -0400

    change loading indicator color to blue

--- src/app/(site)/loading.tsx
+++ src/app/(site)/loading.tsx
@@ -7,7 +7,7 @@ import { Box } from '~/components/ui/legacy/box';
 const Loading = () => {
   return (
     <Box className="flex h-full items-center justify-center">
-      <BounceLoader color="#22c55e" size={40} />
+  <BounceLoader color="#0096FF" size={40} />
     </Box>
   );
 };
